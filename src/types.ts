950e2e1e36360ed17e9d7d6c90803f7a394c1bb1
Fabio Spampinato <spampinabio@gmail.com>
2024-04-05 01:21:11 +0100

    Updated some dependencies

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@
 /* IMPORT */
 
 import type {FSWatcher, BigIntStats} from 'node:fs';
-import type {ResultDirectories} from 'tiny-readdir/dist/types';
+import type {ResultDirectories} from 'tiny-readdir';
 import type {FSTargetEvent, TargetEvent} from './enums';
 import type WatcherStats from './watcher_stats';
 
